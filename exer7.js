96c2f4b04a405b9c5549aa9d8c8f47ad5a8175af
Rickzin1806 <henriquelindaobr@gmail.com>
2025-09-08 09:04:39 -0300

    Create exer7.js

---
+++ exer7.js
@@ -0,0 +1,16 @@
+// Idades das quatro pessoas
+let pessoa1 = 18;
+let pessoa2 = 25;
+let pessoa3 = 32
+let pessoa4 = 40;
+
+// Soma das idades
+let soma = pessoa1 + pessoa2 + pessoa3 + pessoa4;
+
+// Média das idades
+let media = soma / 4;
+
+// Exibindo no console
+console.log("Idades: " + pessoa1 + ", " + pessoa2 + ", " + pessoa3 + ", " + pessoa4);
+console.log("Soma das idades: " + soma);
+console.log("Média das idades: " + media);
