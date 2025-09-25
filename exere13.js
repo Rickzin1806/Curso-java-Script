043480791e5595210531dd06ee61db7542dc23bc
Rickzin1806 <henriquelindaobr@gmail.com>
2025-09-25 12:39:56 -0300

    Create exere13.js

---
+++ exere13.js
@@ -0,0 +1,11 @@
+// Declarando um array de times de futebol
+const times = ["Real Madrid", "Barcelona", "Corinthians", "Flamengo"];
+
+// Acessando e imprimindo apenas o segundo elemento do array
+console.log("O segundo time é:", times[2]); // Saída: O segundo time é: Barcelona
+
+// Adicionando um novo elemento ao final do array
+times.push("Manchester City");
+
+// Imprimindo o array completo
+console.log("Lista de times atualizada:", times);
